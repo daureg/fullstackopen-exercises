34b508b0ab3eb7c71f2d94abedfc1702bebf26ff
Géraud Le Falher <daureg@gmail.com>
2020-04-29 23:19:37 +0200

    step 1a: refactor into several components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (course) => {
+  return ( <>
+    <h1>{course.title}</h1>
+    </>)
+}
+const Total = (exo) => {
+  return ( <>
+    <p>Number of exercises {exo.total}</p>
+    </>)
+}
+
+const Part = (part) => {
+  return ( <>
+    <p>{part.name} {part.num_exercises}</p>
+    </>)
+}
+const Content = (info) => {
+  return (
+    <>
+    <Part name={info.name[0]} num_exercises={info.num_exercises[0]} />
+    <Part name={info.name[1]} num_exercises={info.num_exercises[1]} />
+    <Part name={info.name[2]} num_exercises={info.num_exercises[2]} />
+    </>
+  )
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +37,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <Header title={course} />
+    <Content name={[part1, part2, part3]} num_exercises={[exercises1, exercises2, exercises3]} />
+    <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
