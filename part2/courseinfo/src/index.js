5d6778e59f308102a0ae951ccd93175d6dbe96d9
Géraud Le Falher <daureg@gmail.com>
2020-05-03 18:58:28 +0200

    2.5 move Course to a separate components

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './components/Course'
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((acc, val) => acc + val.exercises, 0)
-  return(
-    <p>Number of exercises {total}</p>
-  ) 
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-    {course.parts.map(p => <Part part={p} key={p.id} />)}
-    </div>
-  )
-}
-const Course = ({course}) => {
-  return (
-  <>
-    <Header course={course} />
-    <Content course={course} />
-    <Total course={course} />
-  </>
-  )
-}
 const App = () => {
   const courses = [ {
     id: 1,
