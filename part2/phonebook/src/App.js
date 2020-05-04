7c80fb9e806809b3ca0244cba248a0db384361c2
Géraud Le Falher <daureg@gmail.com>
2020-05-04 23:21:42 +0200

    2.6 list & add name to the phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form  onSubmit={addName}>
+        <label>
+          name: <input onChange={handleNameChange} type="text" value={newName} />
+        </label>
+          <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+    <ul>
+      {persons.map(p => <li key={p.name}>{p.name}</li>)}
+    </ul>
+    </div>
+  )
+}
+
+export default App
