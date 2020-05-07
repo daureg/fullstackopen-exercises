8a8e3758ddca8af23ab6b6a319bd756f030f4606
Géraud Le Falher <daureg@gmail.com>
2020-05-08 00:20:57 +0200

    2.11 read names from a server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchFilter from './components/SearchFilter'
 import Persons from './components/Persons'
 import AdditionForm from './components/AdditionForm'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
@@ -24,17 +27,10 @@ const App = () => {
     }
     setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setSearch('')
   }
 
   const personsToShow = (search.length > 0) ? persons.filter(p => p.name.toLowerCase().includes(search.toLowerCase())) : persons
-
-  /*
-      <form onSubmit={addName}>
-        <label>name: <input onChange={handleNameChange} type="text" value={newName} /> </label>
-        <label>number: <input onChange={handleNumberChange} type="tel" value={newNumber} /> </label>
-          <button type="submit">add</button>
-      </form>
-  */
 
   return (
     <div>
