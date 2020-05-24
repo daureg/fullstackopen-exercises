88e7a385f1c4c78be5dc56e10a988f2b5c2087fd
Géraud Le Falher <daureg@gmail.com>
2020-05-24 14:41:16 +0200

    4.15 add users and POST/GET for them

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const url = config.MONGODB_URI
 logger.info('connecting to', url.split('@')[1])
@@ -21,6 +22,7 @@ app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
