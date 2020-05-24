e39704c31c12b8816932dede2682f38ad8eb9439
Géraud Le Falher <daureg@gmail.com>
2020-05-24 18:11:06 +0200

    4.19 only let use authenticated user POST blogpost

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const logger = require('../utils/logger')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -7,8 +18,24 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request)
+
+  let decodedToken = { id: null }
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  }
+  catch (e) {
+    logger.error(e)
+  }
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog(request.body)
+  blog.user = user._id
   const result = await blog.save()
+  user.notes = user.blogs.concat(result._id)
+  await user.save()
   response.status(201).json(result)
 })
 
